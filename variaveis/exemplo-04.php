3d2c92dd6b4bc393b7db0bf3e1f56739df28e483
joaonetto <joao.netto@actar.com.br>
2017-06-23 13:46:57 -0300

    15 - Variáveis Pré-Definidas
    Escopo super globais

---
+++ variaveis/exemplo-04.php
@@ -0,0 +1,29 @@
+<?php
+
+  // 15 - Variáveis Pré-Definidas
+  // Escopos super globais
+  //
+  // Para verificar o resultado utilize:
+  // http://localhost/variaveis/exemplo-04.php?a
+  //
+  // Passar o valor 123 no GlobIterator
+  // http://localhost/variaveis/exemplo-04.php?a=123
+  $nome = $_GET['a'];
+  var_dump($nome);
+
+  //Transformando o retorno de A em inteiro
+  $nome = (int)$_GET['a'];
+  var_dump($nome);
+  unset($nome);
+
+  //Recuperando o IP do Cliente
+  $ip = $_SERVER['REMOTE_ADDR'];
+  var_dump($ip);
+  unset($ip);
+
+  //Informacao do Pagina utilizada
+  $pagina = $_SERVER['SCRIPT_NAME'];
+  var_dump($pagina);
+  unset($pagina);
+
+?>
