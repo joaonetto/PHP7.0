cc7b57543309ad05f2822f09b41de4941810a808
joaonetto <joao.netto@actar.com.br>
2017-06-25 13:39:18 -0300

    17 - Operadores PHP
    Parte 1

---
+++ operadores/exemplo-01.php
@@ -0,0 +1,11 @@
+<?php
+
+  //Operadores
+  $nome = 'Teste';
+  // O operador .= retona o valor de $nome e concatena com a nova String
+  // No exemplo abaixo o retorno de nome é: Teste Novo
+  $nome .= ' Novo';
+  echo $nome;
+
+
+?>
