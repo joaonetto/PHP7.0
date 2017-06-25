bdac5007f78dcb95b723254ff6e06eae414d8397
joaonetto <joao.netto@actar.com.br>
2017-06-25 16:49:41 -0300

    17 - Operadores PHP
    Operadores:
    
    ++$ / $++
    —$ / $—

---
+++ operadores/exemplo-07.php
@@ -0,0 +1,30 @@
+<?php
+
+  $azul = '<font face="verdana" size="3" color="blue">';
+  $__clodeFont = '</font>';
+
+  echo $azul;
+
+  // Operadores ++
+  // Adiciona 1 a variável
+  $a = 10;
+  echo 'Valor de $a: ' . $a . '<br>';
+  echo 'Valor de ++$a: ' . ++$a . '<br>';
+  $a++;
+  echo 'Valor de $a++: ' . $a . '<br>';
+  echo '<br>';
+  echo '<br>';
+
+  // Operadores --
+  // Adiciona 1 a variável
+  $a = 10;
+  echo 'Valor de $a: ' . $a . '<br>';
+  echo 'Valor de --$a: ' . --$a . '<br>';
+  $a--;
+  echo 'Valor de $a--: ' . $a . '<br>';
+  echo '<br>';
+  echo '<br>';
+
+  echo $__clodeFont;
+
+?>
