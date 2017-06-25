db9c0b138ee634e1e30ce0e6f513c8b6f227c30e
joaonetto <joao.netto@actar.com.br>
2017-06-25 14:47:21 -0300

    17 - Operadores PHP
    Ajuste Parte 3

--- operadores/exemplo-03.php
+++ operadores/exemplo-03.php
@@ -1,6 +1,7 @@
 <?php
 
   $azul = '<font face="verdana" size="3" color="blue">';
+  $verde = '<font face="verdana" size="3" color="gren">';
   $vermelho = '<font face="verdana" size="3" color="red">';
   $__clodeFont = '</font>';
 
@@ -12,25 +13,29 @@
   echo $azul . 'Valor de $b: ' . $__clodeFont . $vermelho . $b . $__clodeFont . '<br>';
 
   // Soma
-  echo $azul .'Valor da operação: $a + $b: ' . $__clodeFont . $vermelho . intval($a + $b) . $__clodeFont . '<br>';
+  echo $azul .'Valor da operação - Soma: ' . $__clodeFont . $verde . '$a + $b: ' . $__clodeFont . $vermelho . intval($a + $b) . $__clodeFont . '<br>';
 
   // Subtração
-  echo $azul .'Valor da operação: $a - $b: ' . $__clodeFont . $vermelho . intval($a - $b) . $__clodeFont . '<br>';
+  echo $azul .'Valor da operação - Subtração:' . $__clodeFont . $verde . '$a - $b: ' . $__clodeFont . $vermelho . intval($a - $b) . $__clodeFont . '<br>';
 
   // Multiplicação
-  echo $azul .'Valor da operação: $a * $b: ' . $__clodeFont . $vermelho . intval($a * $b) . $__clodeFont . '<br>';
+  echo $azul .'Valor da operação - Multiplicacao:' . $__clodeFont . $verde . '$a * $b: ' . $__clodeFont . $vermelho . intval($a * $b) . $__clodeFont . '<br>';
 
   // Divisão
-  echo $azul .'Valor da operação: $a / $b: ' . $__clodeFont . $vermelho . intval($a / $b) . $__clodeFont . '<br>';
+  echo $azul .'Valor da operação - Divisão:' . $__clodeFont . $verde . '$a / $b: ' . $__clodeFont . $vermelho . intval($a / $b) . $__clodeFont . '<br>';
+  echo '<br>';
+
+  // Elevado
+  echo $azul .'Valor da operação - Elevado:' . $__clodeFont . $verde . '$a ** $b: ' . $__clodeFont . $vermelho . intval($a / $b) . $__clodeFont . '<br>';
   echo '<br>';
 
   // Resto ou Módulo, para caso quando se deseja saber o resto de uma divisão
   $a = 13;
   echo $azul . 'Valor de $a: ' . $__clodeFont . $vermelho . $a . $__clodeFont . '<br>';
   echo $azul . 'Valor de $b: ' . $__clodeFont . $vermelho . $b . $__clodeFont . '<br>';
-  echo $azul . 'Valor da operação: $a / $b - (Quociente): ' . $__clodeFont . $vermelho . intval($a / $b) . $__clodeFont . '<br>';
-  echo $azul . 'Valor da operação: $a % $b - (Resto): ' . $__clodeFont . $vermelho . intval($a % $b) . $__clodeFont . '<br>';
-  echo $azul . 'Valor da operação: $a / $b - (Quociente decimal): ' . $__clodeFont . $vermelho . $a / $b . $__clodeFont . '<br>';
+  echo $azul . 'Valor da operação: ' . $__clodeFont . $verde . '$a / $b - (Quociente): ' . $__clodeFont . $vermelho . intval($a / $b) . $__clodeFont . '<br>';
+  echo $azul . 'Valor da operação: ' . $__clodeFont . $verde . '$a % $b - (Resto): ' . $__clodeFont . $vermelho . intval($a % $b) . $__clodeFont . '<br>';
+  echo $azul . 'Valor da operação: ' . $__clodeFont . $verde . '$a / $b - (Divisão): ' . $__clodeFont . $vermelho . $a / $b . $__clodeFont . '<br>';
 
   echo $__clodeFont;
 ?>
