0f7059739408acfdfd9c3f08dab455ba11ecfdf4
joaonetto <joao.netto@actar.com.br>
2017-06-26 14:12:03 -0300

    23 - Foreach
    Parte 2 com INPUT HTML

---
+++ foreach/exemplo-02.php
@@ -0,0 +1,17 @@
+<form>
+  <input type="text" name="nome">
+  <input type="date" name="nascimento">
+  <input type="submit" value="ok">
+</form>
+
+
+<?php
+
+  if (isset($_GET)) {
+    foreach ($_GET as $key => $value) {
+      echo 'Nome do Campo: ' . $key . '<br>';
+      echo 'Valor do Campo: ' . $value;
+      echo '<hr>';
+    }
+  }
+?>
