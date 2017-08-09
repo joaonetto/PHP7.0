effb7d38124174b6c113411c65aa5a2449c0bf43
actargroup <joao.netto@actar.com.br>
2017-08-09 08:33:59 -0300

    Estado da Sessao

--- session/exemplo-06.php
+++ session/exemplo-06.php
@@ -2,5 +2,20 @@
 
   require_once('config.php');
   echo session_save_path();
+  switch (session_status()) {
+    case PHP_SESSION_DISABLED:
+      $estadoSessao = 'Sessoes desabilitadas.';
+      break;
+
+    case PHP_SESSION_NONE:
+      $estadoSessao = 'Sessoes habilitadas, porem nenhuma existe.';
+      break;
+
+    case PHP_SESSION_ACTIVE:
+      $estadoSessao = 'Sessoes habilitadas, pelo menos 1(uma) existe.';
+      break;
+  }
+
+  echo 'O estado da Sessão é: ' . $estadoSessao;
 
  ?>
