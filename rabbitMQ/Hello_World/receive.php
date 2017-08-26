811d323ef7a2864ee4ef1bc68f2b9635cda101ce
actargroup <joao.netto@actar.com.br>
2017-08-26 04:57:17 -0300

    Inclusão do RabbitMQ

---
+++ rabbitMQ/Hello_World/receive.php
@@ -0,0 +1,30 @@
+<?php
+
+echo __DIR__ . PHP_EOL;
+
+# require_once __DIR__ . '../vendor/autoload.php';
+require_once '../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('10.0.1.5', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+
+$channel->queue_declare('hello', false, false, false, false);
+
+echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+
+$callback = function($msg) {
+  echo " [x] Received ", $msg->body, "\n";
+};
+
+$channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
+
+?>
